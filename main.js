1d50a16f68dbb7fb1c009b9a0638170540b88bbb
kenshinman <archkenny@yahoo.com>
2017-10-12 15:17:21 +0100

    working on plusminus

--- main.js
+++ main.js
@@ -1,25 +1,131 @@
 new Vue({
-    el: "#app",
-    data: {
-        appName: 'VueJS Calculator',
-        values: [],
-        result: '',
+  el: "#app",
+  data: {
+    appName: "VueJS Calculator",
+    values: [],
+    result: "",
+    store: [],
+    restartScreen: false,
+    memory: Number(localStorage.getItem("memory"))
+  },
+  methods: {
+    press: function(e) {
+      e.preventDefault();
+      let value = e.target.innerHTML;
+      if (this.values[0] == "0") {
+        this.values.pop();
+        this.values.push(value);
+      } else if (this.restartScreen) {
+        this.values = [];
+        this.restartScreen = false;
+        this.values.push(value);
+      } else {
+        this.values.push(value);
+      }
+      console.log(this.store);
     },
-    methods: {
-        press: function(e){
-            e.preventDefault();
-            if(this.values == [0]){
-                this.values=[];
-            }
-           this.values.push(e.target.innerHTML);
-            console.log(e.target.innerHTML);
-            
-        },
-        clear: function(){
-            this.values=["0"];
-        }
+    pushToStore: function() {
+      let vals = document.getElementById("values").innerHTML;
+      if (vals.length > 0) {
+        this.store.push(vals);
+      }
+      this.restartScreen = true;
     },
-    mounted: function(){
-        
+    evaluate: function() {
+      let latestVal = document.getElementById("values").innerHTML;
+      this.store.push(latestVal);
+      let storedVals = this.store.join("");
+      var answer = eval(storedVals);
+      this.store = [];
+      this.values = [answer];
+      this.restartScreen = true;
+    },
+    clear: function() {
+      this.values = ["0"];
+      this.store = [];
+      this.restartScreen = false;
+    },
+    divide: function() {
+      this.pushToStore();
+      if (this.store.length > 0) {
+        this.store.push("/");
+      }
+      console.log(this.store);
+    },
+    multiply: function() {
+      this.pushToStore();
+      if (this.store.length > 0) {
+        this.store.push("*");
+      }
+      console.log(this.store);
+    },
+    subtract: function() {
+      this.pushToStore();
+      if (this.store.length > 0) {
+        this.store.push("-");
+      }
+      console.log(this.store);
+    },
+    add: function() {
+      this.pushToStore();
+      if (this.store.length > 0) {
+        this.store.push("+");
+      }
+      console.log(this.store);
+    },
+    equals: function() {
+      this.evaluate();
+    },
+    memoryRecall: function() {
+      let memory = Number(localStorage.getItem("memory"));
+      this.values = [memory];
+      this.restartScreen = true;
+    },
+    memoryAdd: function() {
+      let currentVal = Number(document.getElementById("values").innerHTML);
+      let newMemory = Number(this.memory) + currentVal;
+      localStorage.setItem("memory", newMemory);
+      this.restartScreen = true;
+    },
+    memorySub: function() {
+      let currentVal = Number(document.getElementById("values").innerHTML);
+      let newMemory = Number(this.memory) - currentVal;
+      localStorage.setItem("memory", newMemory);
+      this.restartScreen = true;
+    },
+    squareRoot() {
+      let currentVal = Number(document.getElementById("values").innerHTML);
+      let sqrt = Math.sqrt(currentVal);
+      this.values = [sqrt];
+      this.restartScreen = true;
+    },
+    percentage() {
+      this.pushToStore();
+      if (this.store.length > 0) {
+        this.store.push("/100*");
+      }
+      console.log(this.store);
+    },
+    plusMinus() {
+      let screenVal = document.getElementById("values").innerText;
+      if (this.values[0] != 0) {
+        this.values.unshift("-");
+      } else if (this.values[0] === "-") {
+        this.values.shift();
+      } else {
+        this.values[0] = "-";
+      }
+      console.log(this.values);
+      console.log(this.values[0]);
     }
-})
+  },
+  mounted: function() {
+    let memory = localStorage.getItem("memory");
+    if (!memory) {
+      localStorage.setItem("memory", 0);
+    } else {
+      this.memory = memory;
+    }
+    console.log("memory=>", memory);
+  }
+});
